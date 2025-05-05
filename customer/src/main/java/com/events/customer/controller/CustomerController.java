509f3b3a5835e48370372bcb17130d6984c9618f
Allisson Farias <alissonfilipe@hotmail.com>
2025-05-05 00:45:18 -0300

    feat: configuracao inicial do keycloack

--- customer/src/main/java/com/events/customer/controller/CustomerController.java
+++ customer/src/main/java/com/events/customer/controller/CustomerController.java
@@ -8,6 +8,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -71,6 +72,7 @@ public class CustomerController {
      * @return the created customer DTO
      */
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CustomerDTO> createCustomer(@RequestBody CustomerCreateDTO customerCreateDTO) {
         if (customerService.existsByEmail(customerCreateDTO.getEmail())) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
